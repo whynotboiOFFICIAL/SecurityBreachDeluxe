2c3c269c0dd9e9b1698b96cb0c7d849e9cd76cf0
whynotboi <whynotboiofficial@outlook.com>
2025-04-12 04:51:24 -0400

    Fixed incorrect valid checks

--- lua/autorun/securitybreachautorunnew.lua
+++ lua/autorun/securitybreachautorunnew.lua
@@ -5,7 +5,7 @@ if SERVER then
 
 	
     hook.Add("PlayerButtonDown", "SBNEWSECONDARYNOBUTTONS", function(ply, button)
-        if IsValid(ply:GetNWEntity('2PlayFreddy')) or not IsValid(ply:GetNWEntity('HidingSpotSB')) then return end
+        if not IsValid(ply:GetNWEntity('2PlayFreddy')) and not IsValid(ply:GetNWEntity('HidingSpotSB')) then return end
 
         local ent = ply:GetNWEntity('HidingSpotSB') or ply:GetNWEntity('2PlayFreddy')
 
