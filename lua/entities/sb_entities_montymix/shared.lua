8575b5c86735ae5f6b98c6b1b67dbf8688b76e0e
whynotboi <whynotboiofficial@outlook.com>
2025-04-15 23:08:58 -0400

    The mystery is the pizza flavoring

---
+++ lua/entities/sb_entities_montymix/shared.lua
@@ -0,0 +1,78 @@
+AddCSLuaFile()
+ENT.Type = 'anim'
+
+ENT.PrintName = 'Monty\'s Mystery Mix'
+ENT.Category = 'Security Breach'
+ENT.Model = 'models/whynotboi/securitybreach/base/props/montymysterymix/montymysterymix.mdl'
+
+ENT.Spawnable = true
+ENT.AdminOnly = false
+
+function ENT:Initialize()
+    self:SetModel(self.Model)
+
+    if SERVER then
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetMoveType(MOVETYPE_NONE)
+        self:SetSolid(SOLID_VPHYSICS)
+
+        local phys = self:GetPhysicsObject()
+
+        if (phys:IsValid()) then
+            phys:Wake()
+            phys:EnableDrag(false)
+            phys:SetDamping(0, 0)
+        end
+    end
+end
+
+function ENT:Use(ent)
+end
+
+function ENT:Touch(ent)
+end
+
+function ENT:OnRemove()
+end
+
+function ENT:PhysicsCollide(colData, collider)
+end
+
+local canlure = {
+    drg_sb_glamrockchica = true,
+    drg_sb_ruin_chica = true
+}
+
+local function CanLure(ent)
+    return canlure[ent:GetClass()] == true
+end
+
+function ENT:LureCheck()
+    if self.LureTick or self.BeingDevoured then return end
+
+    self.LureTick = true
+
+    for k,v in pairs(ents.FindInSphere(self:WorldSpaceCenter(), 600)) do
+        if (v == self) or not (v:IsNPC() or v:IsNextBot()) or (v:IsNPC() or v:IsNextBot() and GetConVar('ai_disabled'):GetBool()) or v:Health() < 1 then continue end
+
+        if CanLure(v) and not v.Luring and not v.Stunned then
+            v:LuredTo(self)
+
+            self.BeingDevoured = true
+            break
+        end
+    end
+
+    self:DrG_Timer(1, function()
+        self.LureTick = false
+    end)
+end
+
+function ENT:Think()
+	if ( SERVER ) then
+		self:NextThink( CurTime() )
+        
+        self:LureCheck()
+		return true
+	end
+end
