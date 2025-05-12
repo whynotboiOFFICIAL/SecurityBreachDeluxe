27c8b751699d1714cef3d9aec4b97ee67f80d3cd
entertainment dog <landon.c.winters@gmail.com>
2025-05-11 20:44:18 -0400

    stuff i dunno

--- lua/entities/sb_noisemakerphysics_base/shared.lua
+++ lua/entities/sb_noisemakerphysics_base/shared.lua
@@ -1,9 +1,9 @@
 AddCSLuaFile()
 ENT.Type = 'anim'
 
-ENT.PrintName = 'Noise Maker'
+ENT.PrintName = 'Base Physics Noise Maker'
 ENT.Category = 'Security Breach'
-ENT.Model = 'models/whynotboi/securitybreach/base/props/noisemaker/noisemaker.mdl'
+ENT.Model = 'models/whynotboi/securitybreach/base/props/paintcans/paintcans2.mdl'
 
 ENT.Spawnable = true
 ENT.AdminOnly = false
@@ -13,13 +13,17 @@ ENT.AutomaticFrameAdvance = true
 ENT.SBDistraction = true
 ENT.Toppled = false
 ENT.IsPartofStack = false
-ENT.StackAmount = 2
+ENT.DontSpawnCluster = false
 ENT.NoiseMakerChildren = {}
 
+ENT.StackAmount = 2
+ENT.StackCreateOffset = 10.75
+ENT.StackCreatePOSOffset = 0.33
+
 ENT.UseSounds = {
-    "whynotboi/securitybreach/base/props/noisemaker/daycare/topple/sfx_dynObj_noisemaker_topple_01.wav", 
-    "whynotboi/securitybreach/base/props/noisemaker/daycare/topple/sfx_dynObj_noisemaker_topple_02.wav", 
-    "whynotboi/securitybreach/base/props/noisemaker/daycare/topple/sfx_dynObj_noisemaker_topple_03.wav"
+    "whynotboi/securitybreach/base/props/noisemaker/paintcans/topple/sfx_dynObj_paintCans_topple_01.wav", 
+    "whynotboi/securitybreach/base/props/noisemaker/paintcans/topple/sfx_dynObj_paintCans_topple_02.wav", 
+    "whynotboi/securitybreach/base/props/noisemaker/paintcans/topple/sfx_dynObj_paintCans_topple_03.wav"
 }
 
 ENT.AllSounds = {}
@@ -28,47 +32,44 @@ table.Merge(ENT.AllSounds, ENT.UseSounds)
 
 function ENT:Initialize()
     self:SetModel( self.Model )
-    self:SetSkin( math.random(0, 4) )
     self.SBN_NoiseMakerBase = self
+    self:SetBodygroup( 0, math.random(0, 3) )
 
     if SERVER then
         self:PhysicsInit(SOLID_NONE)
-        self:SetUseType(SIMPLE_USE)
-        self:SetMoveType(MOVETYPE_NONE)
         self:SetSolid(SOLID_VPHYSICS)
-
-        local phys = self:GetPhysicsObject()
-
-        if IsValid(phys) then
-            phys:SetDamping(0, 0)
-        end
+        self:SetMoveType(MOVETYPE_NONE)
+        self:SetUseType(SIMPLE_USE)
 
         if not self.IsPartofStack then
             self:DropToFloor()
-            for i = 1, 2 do
+            for i = 1, self.StackAmount do
                 self:SpawnStack(i)
             end
         end
     end
 end
 
-
 function ENT:SpawnStack(id)
     if CLIENT or self.IsPartofStack then return end
 
-    local stackentry = ents.Create('sb_noisemaker_daycare')
-    stackentry:SetModel('models/whynotboi/securitybreach/base/props/noisemaker/noisemaker.mdl')
-
-    offsetnum = 3 / math.Clamp(id, 1, 3)
-    offset = ( Vector(0, 0, 19.9) * id ) + Vector( math.random(-offsetnum, offsetnum), math.random(-offsetnum, offsetnum), 0 )
+    local stackentry = ents.Create(self:GetClass())
+    
+    offsetnum = self.StackCreatePOSOffset
+    offset = ( Vector(0, 0, self.StackCreateOffset) * id ) + Vector( math.random(-offsetnum, offsetnum), math.random(-offsetnum, offsetnum), 0 )
     stackentry:SetPos(self:GetPos() + offset)
     stackentry:SetAngles( Angle( 0, math.random(0, 359), 0 ) )
 
     stackentry.IsPartofStack = true
     stackentry:Spawn()
+
     stackentry:DrawShadow(false)
 
-    constraint.NoCollide(self, stackentry, 0, 0, true)
+    local phys = stackentry:GetPhysicsObject()
+
+    if IsValid(phys) then
+        phys:SetVelocity(Vector(0,0,0))
+    end
 
     self:DeleteOnRemove(stackentry)
 
@@ -78,42 +79,55 @@ function ENT:SpawnStack(id)
     return stackentry
 end
 
+function ENT:ApplyToppleForce()
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+
+    local calc = ( 25 * math.Rand( 0.80, 1.20 ) )
+
+    local phys = self:GetPhysicsObject()
+
+    if IsValid(phys) then
+        local physcalc = VectorRand(-calc, calc)
+        physcalc.z = 125
+        phys:SetVelocity( physcalc + phys:GetVelocity() )
+        phys:SetAngleVelocity( physcalc + phys:GetAngleVelocity() )
+    end
+
+    return rand
+end
+
 function ENT:Topple()
     local base = IsValid(self.SBN_NoiseMakerBase) and self.SBN_NoiseMakerBase or nil
     local children = IsValid(base) and base.NoiseMakerChildren or nil
+
     if CLIENT or base.Toppled then return end
+
     base.Toppled = true
-    base:ResetSequence("collapse1")
-    base:SetSolid(SOLID_NONE)
     base:EmitSound( base.UseSounds[ math.random(1, 3) ] )
+    base:ApplyToppleForce()
+
     for i = 1, #children do
         local stack = children[i]
-        if i + 1 >= 4 then 
-            stack:SetMoveType(MOVETYPE_VPHYSICS) 
-            stack:SetSolid(SOLID_VPHYSICS)
-            stack:PhysWake()
-        else
-            stack:ResetSequence("collapse"..i + 1)
-            stack:SetPos(base:GetPos())
-            stack:SetSolid(SOLID_NONE)
-        end
+        stack:PhysWake()
+        stack:ApplyToppleForce()
     end
+
 end
 
 function ENT:Use(ent)
     local base = IsValid(self.SBN_NoiseMakerBase) and self.SBN_NoiseMakerBase or nil
+
     if ent:IsPlayer() and base.Toppled then
         ent:PickupObject(self)
-        self:SetMoveType(MOVETYPE_VPHYSICS) 
-        self:SetSolid(SOLID_VPHYSICS)
         self:PhysWake()
-        self:SetSequence("ref")
     end
+
     self:Topple()
 end
 
 local function IsFastEnough(length)
-    return length >= 100
+    return length >= 50
 end
 
 function ENT:Touch(ent)
@@ -125,11 +139,13 @@ end
 function ENT:OnTakeDamage(dmg)
     local base = self.SBN_NoiseMakerBase
     local children = base.NoiseMakerChildren
+
     timer.Simple(0, function()
         if IsValid(base) and IsTableOfEntitiesValid(children) then
             self:Topple()
         end
     end)
+
     self:TakePhysicsDamage(dmg)
 end
 
@@ -137,9 +153,4 @@ function ENT:OnRemove()
     for i = 1, #self.AllSounds do
         self:StopSound(self.AllSounds[i])
     end
-end
-
-function ENT:Think()
-    self:NextThink(CurTime())
-    return true
 end
